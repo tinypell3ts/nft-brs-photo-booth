db70eb3f3fff8d071110c5606af95ae69fc46ecf
Andy <andy.kitt@simpleweb.co.uk>
2022-05-09 13:40:51 +0100

    Add ABIs and simplify codebase

--- constants.ts
+++
@@ -1,8 +0,0 @@
-import NFTAbi from "./abis/nft.json";
-
-export const contracts: Contracts = {
-  nft: {
-    address: process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS ?? "0x",
-    abi: NFTAbi.abi,
-  },
-};
