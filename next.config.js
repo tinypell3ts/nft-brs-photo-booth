01fe1c1e18d405f9e18a6cc8755e94fe16154123
Andy <andy.kitt@simpleweb.co.uk>
2022-05-22 19:38:08 +0100

    ignore eslint during builds

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@ const nextTranslate = require("next-translate");
 
 /** @type {import('next').NextConfig} */
 module.exports = nextTranslate({
-  reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 });
