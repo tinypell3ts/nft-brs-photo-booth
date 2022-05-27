40f6b5a2d7d24f207d04c784a1e254a685b64ef7
Andy <andy.kitt@simpleweb.co.uk>
2022-05-27 22:24:00 +0100

    remove api endpoint, change subgraph to polygon

--- constants.ts
+++ constants.ts
@@ -1,8 +1,6 @@
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const RPC_URL = process.env.RPC_URL;
 const FACTORY_ID = process.env.NEXT_PUBLIC_FACTORY_ID;
 
 const SUBGRAPH_ENDPOINT =
-  "https://api.thegraph.com/subgraphs/name/simpleweb/open-format";
+  "https://api.thegraph.com/subgraphs/name/simpleweb/polygon-open-format";
 
-export { SUBGRAPH_ENDPOINT, RPC_URL, PRIVATE_KEY, FACTORY_ID };
+export { SUBGRAPH_ENDPOINT, FACTORY_ID };
--- pages/photos.tsx
+++ pages/photos.tsx
@@ -144,7 +144,7 @@ export default function Photos() {
             className="invert"
           />
 
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {data.tokens.map((token) => (
               <PhotoItem key={token.id} token={token} />
             ))}
