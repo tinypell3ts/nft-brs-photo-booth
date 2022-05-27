b506379af861f9f5e9af16f7f2228e34a3308352
Benjamin Reid <ben@simpleweb.co.uk>
2022-05-27 16:03:56 +0100

    Remove key warning

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -146,7 +146,7 @@ export default function Photos() {
 
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
             {data.tokens.map((token) => (
-              <PhotoItem token={token} />
+              <PhotoItem key={token.id} token={token} />
             ))}
           </div>
         </div>
